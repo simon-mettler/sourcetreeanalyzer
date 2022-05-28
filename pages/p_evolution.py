fca7bd1479ef07c6d5646d129d523b6c569f4dcd
Simon Mettler <contact@simonmettler.com>
2022-05-28 15:52:13 +0200

    make evolution page graphs update

--- pages/p_evolution.py
+++ pages/p_evolution.py
@@ -15,27 +15,11 @@ files_per_level = {a:'' for a in applications }
 
 # Load statistics from output folder as dataframe.
 for application in applications:
-	release_stats[application] = pd.read_csv(os.path.join(settings.output_dir, application, 'stats_'+application+'.csv'))
-	files_per_level[application] = pd.read_csv(os.path.join(settings.output_dir, application, 'files-per-level_'+application+'.csv'))
+	release_stats[application] = pd.read_csv(
+		os.path.join(settings.output_dir, application, 'stats_'+application+'.csv'))
+	files_per_level[application] = pd.read_csv(
+		os.path.join(settings.output_dir, application, 'files-per-level_'+application+'.csv'))
 
-
-'''
-
-fig_total_files = px.bar(
-	release_stats[current_application], 
-	title = 'total number of files',
-	x = 'release', 
-	y = 'total_files', 
-)
-
-'''
-
-fig_avg_file_size = px.bar(
-	release_stats[current_application], 
-	title = 'avg file size (Bytes)',
-	x = 'release', 
-	y = 'avg_file_size_bytes', 
-)
 
 files_per_level[current_application]['level'] = files_per_level[current_application]['level'].astype(str)
 files_per_level[current_application]['release'] = files_per_level[current_application]['release'].astype(str)
@@ -43,53 +27,11 @@ files_per_level[current_application]['release'] = files_per_level[current_applic
 #print(files_per_level[current_application])
 
 
-fig_avg_folder_size = px.bar(
-	release_stats[current_application], 
-	title = 'avg folder size (num of files in folder)',
-	x = 'release', 
-	y = 'avg_source_folder_size_num_files', 
-) 
-
-fig_max_tree_level = px.line(
-	release_stats[current_application], 
-	title = 'max tree level',
-	x = 'release', 
-	y = 'max_tree_level', 
-) 
-
-fig_tree_width = px.line(
-	release_stats[current_application], 
-	title = '[testing] tree width? -> avg num of files per level',
-	x = 'release', 
-	y = 'avg_num_files_level',
-)
-
-fig_tree_height = px.line(
-	release_stats[current_application], 
-	title = '[testing] tree depth? -> avg depth from root to leaf folder',
-	x = 'release', 
-	y = 'avg_tree_level', 
-)
-
-fig_vertical_growth = px.bar(
-	release_stats[current_application], 
-	title =  '[testing] tree depth growth (%)',
-	x = 'release', 
-	y = 'growth_tree_depth_pct', 
-)
-
-fig_horizontal_growth = px.bar(
-	release_stats[current_application], 
-	title =  '[testing] tree width growth (%)',
-	x = 'release', 
-	y = 'growth_tree_width_pct', 
-)
-
 
 # Page elements
 
-
-# Create dropdown allowing users to select a application.
+# Application Dropdown
+# Allows users to select a application.
 dropdown_options = [] 
 for app in applications:
 	single_option = {
@@ -100,7 +42,7 @@ for app in applications:
 
 app_dropdown = html.Div(
 	[
-		dbc.Label('Select Application', html_for='dropdown'),
+		dbc.Label('Application', html_for='dropdown'),
 		dcc.Dropdown(
 			id = 'dropdown_application',
 			options = dropdown_options,
@@ -118,6 +60,10 @@ def create_fig_total_files(data):
 		title = 'Total number of files',
 		x = 'release', 
 		y = 'num_files', 
+		labels = {
+			'release': 'Release',
+			'num_files': 'Number of files',
+		},
 	)
 	return fig
 
@@ -130,12 +76,61 @@ def create_fig_files_per_level(data):
 		y = 'num_files', 
 		color = 'level', 
 		markers = True,
+		labels = {
+			'release': 'Release',
+			'num_files': 'Number of files',
+			'level': 'Tree level',
+		},
 	)
 
-	# Reset category order after grouping.
+	# Set category order after grouping.
 	unique_releases = data['release'].unique().tolist()
-	fig.update_xaxes(categoryorder = 'array', categoryarray = unique_releases) 
+	fig.update_xaxes(
+		categoryorder = 'array', 
+		categoryarray = unique_releases, 
+		range = [0, len(data['release'].unique())-1]
+	) 
 	
+	return fig
+
+
+def create_fig_avg_file_size_bytes(data):
+	fig = px.line(
+		data,
+		title = 'Average file size in Bytes',
+		x = 'release', 
+		y = 'avg_file_size_bytes', 
+		labels = {
+			'release': 'Release',
+			'avg_file_size_bytes': 'Avg. file size',
+		},
+	)
+	return fig
+
+def create_fig_avg_folder_size(data):
+	fig = px.bar(
+		data,
+		title = 'Average source folder size (number of files in folder)',
+		x = 'release', 
+		y = 'avg_source_folder_size_num_files', 
+		labels = {
+			'release': 'Release',
+			'avg_source_folder_size_num_files': 'Avg. folder size (num. of files in folder)',
+		},
+	) 
+	return fig
+
+def create_fig_max_tree_level(data):
+	fig = px.line(
+		data,	
+		title = 'Maximum tree level',
+		x = 'release', 
+		y = 'max_tree_level', 
+		labels = {
+			'release': 'Release',
+			'max_tree_level': 'Maximum tree level',
+		},
+	) 
 	return fig
 
 
@@ -147,37 +142,50 @@ def create_fig_files_per_level(data):
 	Input('dropdown_application', 'value')
 )
 def update_selected_app(input_value):
-	return input_value
+	if input_value:
+		return input_value
+	else: 
+		return 'Please select an application'
 
 
 # Updates figures based on selected application.
 @callback(
 	Output('fig-total-files', 'figure'),
 	Output('fig-files-per-level', 'figure'),
+	Output('fig-avg-file-size-bytes', 'figure'),
+	Output('fig-avg-folder-size', 'figure'),
+	Output('fig-max-tree-level', 'figure'),
 	Input('dropdown_application', 'value')
 )
 def update_figure(selected_value):
 	if selected_value:
 		data_release = release_stats[selected_value]
 		data_fpl = files_per_level[selected_value]
-		return create_fig_total_files(data_release), create_fig_files_per_level(data_fpl)
+		return (
+			create_fig_total_files(data_release), 
+			create_fig_files_per_level(data_fpl), 
+			create_fig_avg_file_size_bytes(data_release),
+			create_fig_avg_folder_size(data_release),
+			create_fig_max_tree_level(data_release),
+		)
 	else:
-		data = pd.DataFrame.from_dict({'release': [], 'num_files': [], 'level': []})
-		return create_fig_total_files(data), create_fig_files_per_level(data)
-
+		data = pd.DataFrame.from_dict({'release': [], 'num_files': [], 'level': [], 'avg_file_size_bytes': [], 'avg_source_folder_size_num_files': [], 'max_tree_level': []})
+		return (
+			create_fig_total_files(data), 
+			create_fig_files_per_level(data),
+			create_fig_avg_file_size_bytes(data),
+			create_fig_avg_folder_size(data),
+			create_fig_max_tree_level(data),
+		)
 
 
 # Dash page layout.
 layout = dbc.Container([
 	html.H1('', id = 'page-title'),
 	app_dropdown,
-	dcc.Graph( id='fig-total-files' ),
-	dcc.Graph( id='fig-files-per-level'), 
-	dcc.Graph( id='avg_file_size', figure = fig_avg_file_size),
-	dcc.Graph( id='avg_folder_size', figure = fig_avg_folder_size),
-	dcc.Graph( id='max_tree_level', figure = fig_max_tree_level),
-	dcc.Graph( id='tree_width', figure = fig_tree_width),
-	dcc.Graph( id='tree_height', figure = fig_tree_height),
-	dcc.Graph( id='horizontal_growth', figure = fig_horizontal_growth),
-	dcc.Graph( id='vertical_growth', figure = fig_vertical_growth),
+	dcc.Graph(id='fig-total-files' ),
+	dcc.Graph(id='fig-files-per-level'), 
+	dcc.Graph(id='fig-avg-file-size-bytes'),
+	dcc.Graph(id='fig-avg-folder-size'),
+	dcc.Graph(id='fig-max-tree-level')
 ])
