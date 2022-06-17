df298b4cf9bbfb7dfc72aae7ab57b2e01bf4e0a5
Simon Mettler <contact@simonmettler.com>
2022-06-17 18:34:18 +0200

    update metrics

--- analyze.py
+++ analyze.py
@@ -23,22 +23,28 @@ for application in applications:
 		'release': [],
 		'release_size_bytes': [],
 		'num_files': [],
+		'max_file_size_bytes': [],
 		'avg_file_size_bytes': [],
 		'num_folders': [],
 		'num_source_folders': [],
+		'max_source_folder_size_num_files': [],
 		'avg_source_folder_size_num_files': [],
 		'avg_source_folder_size_bytes': [],
 		'max_tree_level': [],
 		'avg_tree_level': [],
 		'max_num_files_level': [],
 		'avg_num_files_level': [],
-		'tree_size': []
+		'max_num_folders_level': [],
+		'avg_num_folders_level': [],
+		'tree_size': [],
+		'metric_source_folder': [],
 	}
 
-	files_per_level = {
+	level_stats = {
 		'release': [],
 		'level': [],
 		'num_files': [],
+		'num_folders': [],
 	}
 
 
@@ -77,7 +83,7 @@ for application in applications:
 		fs['depth'] = fs['depth'] + 1
 
 
-		# Start path at root release folder.
+		# Cut path to start at root release folder.
 		def cut_path(p):
 			path = pathlib.Path(p)
 			return path.relative_to(*path.parts[:3])
@@ -85,18 +91,17 @@ for application in applications:
 		fs['path'] = fs['path'].apply(cut_path)
 
 		# Create hash from path.
+		# This ensures consistent id's even if nodes are added to the folder tree.
 		def hash_path(p):
 			hash_value = hashlib.md5(str(p).encode('utf-8')).hexdigest()
 			return hash_value
 
 		fs['hash_id'] = fs['path'].apply(hash_path)
 		
-
 		fs = pd.merge(fs, fs[['hash_id', 'id']], left_on = 'parent', right_on = 'id', how = 'left')
 
 		# Delete unused columns.
 		fs.drop(columns=['id_y','id_x', 'parent', 'uid', 'ctime', 'atime',  'path'], inplace=True)
-
 
 		# Rename columns
 		fs.rename(columns = {
@@ -109,20 +114,27 @@ for application in applications:
 		}, 
 		inplace = True)
 
+		# Removes all folders which do not contain at least one source file in its subtree.
+		fs = fs.drop(fs[(fs['folder'] == True) & (fs['num_files'] == 0)].index)
+
 		# Export detailed release statistics to csv.
 		fs.to_csv(
 			os.path.join(settings.output_dir, application, 'tree_' + release + '.csv'), 
 			index = False
 		)
 
+
 		# Calculate release metrics.
 
-		# All source folders.
+		# Total number of source folders.
 		num_source_folders = fs.loc[ fs['id'].isin(fs[fs['folder'] == False]['parent'])]
 		release_stats['num_source_folders'].append(len(num_source_folders))
 
+		# Max source folder size (number of files)
+		release_stats['max_source_folder_size_num_files'].append(fs['num_files_direct'].max())
+
 		# Average tree level.
-		levels = fs.loc[ (fs['folder'] == True) & (~fs['id'].isin(fs['parent'])) ] # Gets all leaf nodes (folders).
+		levels = fs.loc[ (fs['folder'] == True) & (~fs['id'].isin(fs['parent'])) ] # Gets all folder leaf nodes.
 		avg_tree_level = round(levels['level'].mean(), 4)
 		release_stats['avg_tree_level'].append(avg_tree_level)
 
@@ -136,42 +148,65 @@ for application in applications:
 		release_size_bytes = fs.loc[fs['folder'] == False, ['size_bytes']].sum()['size_bytes']
 		release_stats['release_size_bytes'].append(release_size_bytes)
 
+		# Max file size in bytes.
+		release_stats['max_file_size_bytes'].append(
+			fs.loc[fs['folder'] == False, ['size_bytes']].max()['size_bytes'])
+
 		# Average file size in bytes.
-		release_stats['avg_file_size_bytes'].append(round(fs.loc[fs['folder'] == False, ['size_bytes']].mean()['size_bytes'], 2))
+		release_stats['avg_file_size_bytes'].append(
+			round(fs.loc[fs['folder'] == False, ['size_bytes']].mean()['size_bytes'], 2))
+
 
 		# Average folder size (number of files in folder).
-		release_stats['avg_source_folder_size_num_files'].append(round(num_files / num_source_folders.shape[0], 2))
+		release_stats['avg_source_folder_size_num_files'].append(
+			round(num_files / num_source_folders.shape[0], 2))
 
 		# Average folder size (bytes).
-		release_stats['avg_source_folder_size_bytes'].append(round(release_size_bytes / num_source_folders.shape[0], 0))
+		release_stats['avg_source_folder_size_bytes'].append(
+			round(release_size_bytes / num_source_folders.shape[0], 0))
 		
 		# Maximum tree level.
-		release_stats['max_tree_level'].append(fs['level'].max())
+		max_tree_level = fs.loc[fs['folder'] == True]['level'].max()
+		release_stats['max_tree_level'].append(max_tree_level)
+
+		# Calculate Metrics.
+		release_stats['metric_source_folder'].append(round(num_source_folders.shape[0] / num_files, 2))
 
 
-		# Number of files per level. 
-		fpl = fs.loc[fs['folder'] == False].value_counts('level').to_frame().reset_index()
-		fpl.columns = ['level', 'num_files']
-		fpl['release'] = release
-		fpl.sort_values(by = 'level', ascending = True, inplace = True)
+		# Number of files and folders per level. 
+		files = fs.loc[fs['folder'] == False].value_counts('level').to_frame().reset_index()
+		files.columns = ['level', 'num_files']
+		files['release'] = release
+
+		folders = fs.loc[fs['folder'] == True].value_counts('level').to_frame().reset_index()
+		folders.columns = ['level', 'num_folders']
+		folders['release'] = release
+
+		files_folders = pd.merge(folders, files, on=['level', 'release'], how = 'outer')
+		files_folders.sort_values(by = 'level', ascending = True, inplace = True)
 		
-		files_per_level['release'] += fpl['release'].to_list()
-		files_per_level['level'] += fpl['level'].to_list()
-		files_per_level['num_files'] += fpl['num_files'].to_list()
+		level_stats['release'] += files_folders['release'].to_list()
+		level_stats['level'] += files_folders['level'].to_list()
+		level_stats['num_files'] += files_folders['num_files'].to_list()
+		level_stats['num_folders'] += files_folders['num_folders'].to_list()
 
-		# Max number of files per level.
-		release_stats['max_num_files_level'].append(round(fpl['num_files'].max(), 2))
+		# Max number of files and folders per level.
+		release_stats['max_num_files_level'].append(files_folders['num_files'].max())
+		max_num_folders_level = files_folders['num_folders'].max()
+		release_stats['max_num_folders_level'].append(max_num_folders_level)
 
-		# Average number of files per level.
-		avg_num_files_level = round(fpl['num_files'].mean(), 2)
-
+		# Average number of files and folders per level.
+		avg_num_files_level = round(files_folders['num_files'].mean(), 2)
+		avg_num_folders_level = round(files_folders['num_folders'].mean(), 2)
 		release_stats['avg_num_files_level'].append(avg_num_files_level)
-		release_stats['tree_size'].append(avg_num_files_level/avg_tree_level)
+		release_stats['avg_num_folders_level'].append(avg_num_folders_level)
+
+		release_stats['tree_size'].append(max_tree_level/max_num_folders_level)
 
 
 	# Convert dicts to dataframes.
 	df_release_stats = pd.DataFrame.from_dict(release_stats)
-	df_files_per_level = pd.DataFrame.from_dict(files_per_level)
+	df_level_stats = pd.DataFrame.from_dict(level_stats)
 
 
 	# Calculate growth rates from different metrics.
@@ -195,11 +230,16 @@ for application in applications:
 	df_release_stats['growth_avg_num_files_level_pct'] = pct_growth(df_release_stats['avg_num_files_level'])
 	df_release_stats['growth_tree_depth_pct'] = pct_growth(df_release_stats['avg_tree_level'])
 	df_release_stats['growth_tree_width_pct'] = pct_growth(df_release_stats['avg_num_files_level'])
+	df_release_stats['growth_tree_size_pct'] = pct_growth(df_release_stats['tree_size'])
 
 
 	# Export results to csv.
-	df_release_stats.to_csv(os.path.join(settings.output_dir, application, 'stats_' + application + '.csv'), index = False)
-	df_files_per_level.to_csv(os.path.join(settings.output_dir, application, 'files-per-level_' + application + '.csv'), index = False)
+	df_release_stats.to_csv(
+		os.path.join(settings.output_dir, application, 'stats_' + application + '.csv'), 
+		index = False)
+	df_level_stats.to_csv(
+		os.path.join(settings.output_dir, application, 'files-per-level_' + application + '.csv'), 
+		index = False)
 
 
 	print(f"Exported {application} results to csv")
--- settings.py
+++ settings.py
@@ -1,4 +1,37 @@
 input_dir = 'input'
+
 output_dir = 'output'
-file_extensions = ['py', 'java', 'h', 'c', 'cpp']
-exclude = ['benchmarks', 'androidTest', 'test', 'docs']
+
+file_extensions = [
+	'py', 
+	'java', 
+	'h', 
+	'c', 
+	'cc',
+	'cpp'
+]
+
+exclude = [
+	'androidTest', 
+	'animated-base-test', # fresco
+	'asv-bench' # pandas
+	'benchmarks', 
+	'cmake', 
+	'demo',
+	'demos',
+	'doc',
+	'docs',
+	'doc-src',
+	'docker-compose',
+	'docker-files',
+	'examples',
+	'imagepipeline-base-test', # fresco
+	'imagepipeline-test', # fresco
+	'manual',
+	'samples',
+	'snap',
+	'test', 
+	'tests', 
+	'unittest',
+	'wiki',
+]
